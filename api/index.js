bdea4829b6bd82ba0a8ab38e48de03a09e152062
WaTaShi <jm.pasamba5@gmail.com>
2026-07-13 16:37:09 +0800

    Fix POST / Cannot POST / - switch from rewrites to routes format; Express serves /static/* via express.static so CSS works regardless

--- api/index.js
+++ api/index.js
@@ -3,10 +3,6 @@ const { app, initializeDatabase } = require("../src/server");
 let initialized = false;
 
 module.exports = async (req, res) => {
-  console.log("VERCEL_URL:", req.url);
-  console.log("VERCEL_METHOD:", req.method);
-  console.log("VERCEL_HEADERS:", JSON.stringify({ host: req.headers.host, "x-forwarded-host": req.headers["x-forwarded-host"], "x-forwarded-proto": req.headers["x-forwarded-proto"] }));
-
   if (!initialized) {
     try {
       await initializeDatabase();
@@ -15,7 +11,6 @@ module.exports = async (req, res) => {
       console.error("DB init error on cold start:", err.message);
     }
   }
-
   await new Promise((resolve) => {
     res.on("finish", resolve);
     app(req, res);
